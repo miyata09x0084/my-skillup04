1eb6a0b5cd53ff9f2a8e12af8fd229f7feb50cbe
miyata09x0084 <59190800+miyata09x0084@users.noreply.github.com>
2021-08-10 15:24:04 +0900

    作成した球体に地球のテクスチャ画像をマッピングする

--- main.js
+++ main.js
@@ -4,16 +4,14 @@ import * as THREE from 'https://cdn.skypack.dev/three@0.126.1';
 window.addEventListener('load', init);
 
 function init() {
-
   // サイズを指定
   const width = 960;
   const height = 540;
 
   // レンダラーを作成
   const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#myCanvas')
+    canvas: document.querySelector('#myCanvas'),
   });
-  renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(width, height);
 
   // シーンを作成
@@ -23,18 +21,32 @@ function init() {
   const camera = new THREE.PerspectiveCamera(45, width / height);
   camera.position.set(0, 0, +1000);
 
-  // 箱を作成
-  const geometry = new THREE.BoxGeometry(400, 400, 400);
-  const material = new THREE.MeshNormalMaterial();
-  const box = new THREE.Mesh(geometry, material);
-  scene.add(box);
+  // 球体を作成
+  const geometry = new THREE.SphereGeometry(300, 30, 30);
+  // 画像を読み込む
+  const loader = new THREE.TextureLoader();
+  const texture = loader.load('imgs/875earth.jpeg');
+  // マテリアルにテクスチャーを設定
+  const material = new THREE.MeshStandardMaterial({
+    map: texture,
+  });
+  // メッシュを作成
+  const mesh = new THREE.Mesh(geometry, material);
+  // 3D空間にメッシュを追加
+  scene.add(mesh);
+
+  // 平行光源
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(1, 1, 1);
+  // シーンに追加
+  scene.add(directionalLight);
 
   tick();
 
   // 毎フレーム時に実行されるループイベントです
   function tick() {
-    box.rotation.y += 0.01;
-    renderer.render(scene, camera); // レンダリング
+    // レンダリング
+    renderer.render(scene, camera);
 
     requestAnimationFrame(tick);
   }
